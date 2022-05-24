6f500dc55b4b6847161fc8de595396ef09b75e6c
Avery Uslaner <uslaner.avery@gmail.com>
2022-05-23 18:05:49 -0600

    get current condition

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+
+from weather import get_current_condition
+
+
 def main():
-    pass
+    current = get_current_condition()
+    print(current)
 
 if __name__ == '__main__':
     main()
--- weather.py
+++ weather.py
@@ -2,12 +2,16 @@ import os
 
 import requests
 
+# Possible weather conditions: https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 LATITUDE = os.getenv('LATITUDE')
 LONGITUDE = os.getenv('LONGITUDE')
 
+def get_current_condition():
+    weather = get_weather().json()
+    return weather['weather'][0]['main']
 
 def get_weather():
-    resp = requests.get(
-        f"https://api.openweathermap.org/data/2.5/onecall?lat={LATITUDE}&lon={LONGITUDE}&APPID={WEATHER_API_KEY}")
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={LATITUDE}&lon={LONGITUDE}&appid={WEATHER_API_KEY}"
+    resp = requests.get(url)
     return resp
