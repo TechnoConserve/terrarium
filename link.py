1f9b804f9dec672b5733a1d7b13d3fe3e035a7b4
Avery Uslaner <uslaner.avery@gmail.com>
2022-05-24 14:01:53 -0600

    Use pin to indicate on/off to arduino

--- link.py
+++ link.py
@@ -6,17 +6,26 @@ except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
 THUNDERSTORM_GPIO_PIN = 16
+ON_OFF_PIN = 23  # Tell arduino if lights should be on or off
 
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(THUNDERSTORM_GPIO_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(ON_OFF_PIN, GPIO.OUT, initial=GPIO.HIGH)
     
     atexit.register(exit_handler)
 
 def indicate_thunderstorm():
     GPIO.output(THUNDERSTORM_GPIO_PIN, GPIO.HIGH)
 
+def indicate_off():
+    GPIO.output(ON_OFF_PIN, GPIO.LOW)
+
+def indicate_on():
+    GPIO.output(ON_OFF_PIN, GPIO.HIGH)
+
 def set_low():
+    """Set weather indicators to LOW"""
     GPIO.output(THUNDERSTORM_GPIO_PIN, GPIO.LOW)
 
 def exit_handler():
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from link import indicate_thunderstorm, setup, set_low
+import datetime as dt
+
+from link import indicate_on, indicate_off, indicate_thunderstorm, setup, set_low
 from weather import get_current_condition
 
+SALT_LAKE_CITY_TIME_OFFSET = -6
+SLC_TZ_INFO = dt.timezone(dt.timedelta(hours=SALT_LAKE_CITY_TIME_OFFSET))
+
+def in_between(now, start, end):
+    """https://stackoverflow.com/a/33681543/1175701"""
+    if start <= end:
+        return start <= now < end
+    else: # over midnight e.g., 23:30-04:15
+        return start <= now or now < end
 
 def main():
     setup()
-    current = get_current_condition()
 
-    # Set everything back to low
-    set_low()
-    
-    if current == 'Thunderstorm':
-        indicate_thunderstorm()
+    # If daytime, do the things
+    if not in_between(now=dt.datetime.now(SLC_TZ_INFO).time(), start=dt.time(23), end=dt.time(7, 30)):
+        indicate_on()
+        current = get_current_condition()
+
+        # Set everything back to low
+        set_low()
+        
+        if current == 'Thunderstorm':
+            indicate_thunderstorm()
+
+    else:
+        # Tell arduino the lights should be off
+        indicate_off()
+
 
 if __name__ == '__main__':
     main()
