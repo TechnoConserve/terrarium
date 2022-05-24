219734689f4cac6f6b30b196e2a563f83a1adb85
Avery Uslaner <uslaner.avery@gmail.com>
2022-05-24 12:26:22 -0600

    Set everything low before checking weather

--- link.py
+++ link.py
@@ -1,18 +1,23 @@
 # Communicate with the arduino controlling the lights using the GPIO pins
+import atexit
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
-THUNDERSTORM_GPIO_PIN = 14
+THUNDERSTORM_GPIO_PIN = 16
 
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(THUNDERSTORM_GPIO_PIN, GPIO.OUT, initial=GPIO.LOW)
+    
+    atexit.register(exit_handler)
 
 def indicate_thunderstorm():
     GPIO.output(THUNDERSTORM_GPIO_PIN, GPIO.HIGH)
 
-def reset():
-    """Set all pins to LOW"""
+def set_low():
     GPIO.output(THUNDERSTORM_GPIO_PIN, GPIO.LOW)
+
+def exit_handler():
+    GPIO.cleanup()
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import os
-
-from link import indicate_thunderstorm, setup
+from link import indicate_thunderstorm, setup, set_low
 from weather import get_current_condition
 
 
 def main():
     setup()
     current = get_current_condition()
+
+    # Set everything back to low
+    set_low()
     
     if current == 'Thunderstorm':
         indicate_thunderstorm()
